d404761dd5ba2b905f31e5d8fb5a1ea7db53156f
mohammedMesbahi <mohammed.mesbahiaouam@usmba.ac.ma>
2022-11-19 17:12:16 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace ADO.NET_PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpDate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDisplayAll = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxCin = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblCin = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnInsert, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnUpDate, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnDelete, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnDisplayAll, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(481, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(316, 284);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInsert.Location = new System.Drawing.Point(3, 3);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(310, 65);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpDate
+            // 
+            this.btnUpDate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpDate.Location = new System.Drawing.Point(3, 74);
+            this.btnUpDate.Name = "btnUpDate";
+            this.btnUpDate.Size = new System.Drawing.Size(310, 65);
+            this.btnUpDate.TabIndex = 1;
+            this.btnUpDate.Text = "Up Date";
+            this.btnUpDate.UseVisualStyleBackColor = true;
+            this.btnUpDate.Click += new System.EventHandler(this.btnUpDate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(3, 145);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(310, 65);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnDisplayAll
+            // 
+            this.btnDisplayAll.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDisplayAll.Location = new System.Drawing.Point(3, 216);
+            this.btnDisplayAll.Name = "btnDisplayAll";
+            this.btnDisplayAll.Size = new System.Drawing.Size(310, 65);
+            this.btnDisplayAll.TabIndex = 3;
+            this.btnDisplayAll.Text = "Display All";
+            this.btnDisplayAll.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 355);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(795, 214);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(140, 272);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(152, 37);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(20, 123);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(377, 26);
+            this.textBoxFirstName.TabIndex = 3;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(20, 218);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(377, 26);
+            this.textBoxLastName.TabIndex = 4;
+            // 
+            // textBoxCin
+            // 
+            this.textBoxCin.Location = new System.Drawing.Point(20, 50);
+            this.textBoxCin.Name = "textBoxCin";
+            this.textBoxCin.Size = new System.Drawing.Size(377, 26);
+            this.textBoxCin.TabIndex = 5;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(20, 98);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(86, 20);
+            this.lblFirstName.TabIndex = 6;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(20, 192);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(86, 20);
+            this.lblLastName.TabIndex = 7;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblCin
+            // 
+            this.lblCin.AutoSize = true;
+            this.lblCin.Location = new System.Drawing.Point(20, 27);
+            this.lblCin.Name = "lblCin";
+            this.lblCin.Size = new System.Drawing.Size(36, 20);
+            this.lblCin.TabIndex = 8;
+            this.lblCin.Text = "CIN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 569);
+            this.Controls.Add(this.lblCin);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.textBoxCin);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpDate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDisplayAll;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxCin;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblCin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ADO.NET_PROJECT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection(@"Data source=NORD\MSSQLSERVER01;initial catalog=ADONET; integrated security =true;");
+        SqlCommand command;
+        string queryString;
+        SqlDataReader reader;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            queryString = "select count(*) from citizens";
+            command = new SqlCommand(queryString, connection);
+            try
+            {
+                connection.Open();
+                int numberOfRows = (Int32)command.ExecuteScalar();
+                if (numberOfRows == 0)
+                {
+                    btnDelete.Enabled = false;
+                    btnDisplayAll.Enabled = false;
+                    btnUpDate.Enabled = false;
+                }
+                connection.Close();
+            }
+            catch
+            {
+                MessageBox.Show("an Error accured");
+            }
+        }
+        private bool IsinputsValid()
+        {
+            // Check for input in the Order ID text box.
+            if (textBoxCin.Text == "")
+            {
+                MessageBox.Show("Please specify the CIN.");
+                return false;
+            }
+
+            // Check for characters other than integers.
+            else if (textBoxFirstName.Text == "")
+            {
+                // Show message and clear input.
+                MessageBox.Show("Please specify the First name");
+                return false;
+            }
+            else if (textBoxLastName.Text == "")
+            {
+                MessageBox.Show("Please specify the Last name");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private void btnUpDate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("you clicked me");
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string message = "Do you want to continue?";
+            string title = "Close Window";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                if (IsinputsValid())
+                {
+                    queryString = "INSERT INTO citizens VALUES(@CIN,@FirstName,@LastName);";
+                    command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@CIN", SqlDbType.VarChar);
+                    command.Parameters["@CIN"].Value = textBoxCin.Text;
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters["@FirstName"].Value = textBoxFirstName.Text;
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters["@LastName"].Value = textBoxLastName.Text;
+                    try
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        queryString = "select * from citizens";
+                        command = new SqlCommand(queryString, connection);
+                        reader = command.ExecuteReader();
+                        // Create a data table to hold the retrieved data.
+                        DataTable dataTable = new DataTable();
+
+                        // Load the data from SqlDataReader into the data table.
+                        dataTable.Load(reader);
+
+                        // Display the data from the data table in the data grid view.
+                        this.dataGridView1.DataSource = dataTable;
+
+                        // Close the SqlDataReader.
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        MessageBox.Show("an Error accured");
+                    }
+                }
+
+
+            }
+            else
+            {
+                // Do something  
+
+            }
+
+        }
+    }
+}
